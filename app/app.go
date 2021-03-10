935157e3030f645a901000ec706defc2db3855b3
Aiuzu <aiuzu42@gmail.com>
2021-03-09 20:40:56 -0600

    V2.1.1.0 fixed bug that was preventing guild member updates

--- app/app.go
+++ app/app.go
@@ -32,6 +32,7 @@ func StartApp() {
 	if err != nil {
 		log.Fatal("[StartApp]Error selecting repository: " + err.Error())
 	}
+	AppSession.b.Identify.Intents = discordgo.IntentsAll
 	AppSession.b.AddHandler(bot.CommandsHandler)
 	AppSession.b.AddHandler(bot.NewMemberHandler)
 	AppSession.b.AddHandler(bot.MemberLeaveHandler)
